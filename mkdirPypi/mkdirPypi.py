ce18694382c3591b58511715a671abbc120ef381
batestin1 <mayconcipriano@gmail.com>
2022-03-10 22:02:25 -0300

    v1 -20221003

--- mkdirPypi/mkdirPypi.py
+++ mkdirPypi/mkdirPypi.py
@@ -14,6 +14,7 @@
                         #********************************************************************************#
 
 from pathlib import Path
+from datetime import date
 
 def mkdirPypi(filename):
     #create a home directory#
@@ -23,14 +24,14 @@ def mkdirPypi(filename):
         }
     path = Path(f"./{filename}")
     path.mkdir(parents=True, exist_ok=True)
+    data_atual = date.today()
+    data = f"""created: {data_atual.strftime('%Y-%m-%d')}"""
 
     #### create a LICENSE ####
     textLic ="""
-    MIT License
-
-    Copyright (c) 2018 Yan Orestes
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+MIT License
+Copyright (c) 2018 Yan Orestes
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
     """
     licensa = open(f"{filename}/LICENSE", "w")
     licensa.write(textLic)
@@ -38,31 +39,20 @@ def mkdirPypi(filename):
     #### create a README.md ###
 
     textReadm = f"""
-    <h1 align="center">
-    <img src="https://img.shields.io/static/v1?label={filename.upper()}%20POR&message=MAYCON%20BATESTIN&color=7159c1&style=flat-square&logo=ghost"/>
-
-
-    <h3> <p align="center">{filename.upper()} </p> </h3>
-    <h3> <p align="center"> ================= </p> </h3>
-
-    >> <h3> Resume </h3>
-
-    <p> text here </p>
-
-    >> <h3> How install </h3>
-
-    ```
-    code here
-
-    ```
-
-    >> <h3> How Works </h3>
-
-    ```
-    code here
-
-    ```
-
+<h1 align="center">
+<img src="https://img.shields.io/static/v1?label={filename.upper()}%20POR&message=MAYCON%20BATESTIN&color=7159c1&style=flat-square&logo=ghost"/>
+<h3> <p align="center">{filename.upper()} </p> </h3>
+<h3> <p align="center"> ================= </p> </h3>
+>> <h3> Resume </h3>
+<p> text here </p>
+>> <h3> How install </h3>
+```
+code here
+```
+>> <h3> How Works </h3>
+```
+code here
+```
     """
     readme = open(f"{filename}/README.md", "w")
     readme.write(textReadm)
@@ -70,31 +60,30 @@ def mkdirPypi(filename):
     ###setup.cfg###
 
     cfgTxt = """
-    [metadata]
-    description-file = README.md
-    license_file = LICENSE.txt
-    """
+[metadata]
+description-file = README.md
+license_file = LICENSE.txt
+"""
     cfgsetup = open(f"{filename}/setup.cfg", "w")
     cfgsetup.write(cfgTxt)
 
     ###setup.py ######
 
     setupyT = f"""
-    from setuptools import setup
-
-    setup(
-        name = '{filename}',
-        version = '1.0.0',
-        author = 'Maycon Cypriano Batestin',
-        author_email = 'mayconcipriano@gmail.com',
-        packages = ['{filename}'],
-        description = 'a way to make your life easier',
-        long_description = 'file: README.md',
-        url = 'https://github.com/batestin1/',
-        project_urls = {project_urls},
-        keywords = 'a way to make your life easier',
-        classifiers = []
-    )"""
+from setuptools import setup
+setup(
+    name = '{filename}',
+    version = '1.0.0',
+    author = 'Maycon Cypriano Batestin',
+    author_email = 'mayconcipriano@gmail.com',
+    packages = ['{filename}'],
+    description = 'a way to make your life easier',
+    long_description = 'file: README.md',
+    url = 'https://github.com/batestin1/',
+    project_urls = {project_urls},
+    keywords = 'a way to make your life easier',
+    classifiers = []
+)"""
 
     setupy = open(f"{filename}/setup.py", "w")
     setupy.write(setupyT)
@@ -106,20 +95,18 @@ def mkdirPypi(filename):
     txtnull=f"""
                             #********************************************************************************#
                             #                                                                                #
-                            #                                  hello bates                                   #
+                            #                                  hello                                         #
                             #                                                                                #
                             #   filename:{filename}.py                                                       #
-                            #   created: 2022-03-10                                                          #
-                            #   system: Windows                                                              #
-                            #   version: 64bit                                                               #
-                            #                                       by: Bates <https://github.com/batestin1> #
+                            #   created: {data}                                                              #
+                            #                                                                                #
                             #********************************************************************************#
                             #                           import your librarys below                           #
                             #********************************************************************************#
 
 
-    def {filename}():
-        pass
+def {filename}():
+    pass
     """
 
     main = open(f"{filename}/{filename}/{filename}.py", "w")
@@ -129,19 +116,17 @@ def mkdirPypi(filename):
     txtnull2=f"""
                             #********************************************************************************#
                             #                                                                                #
-                            #                                  Hello, Bates                                  #
+                            #                                  hello                                         #
                             #                                                                                #
-                            #   filename: __init__.py                                                        #
-                            #   created: 2022-03-10                                                          #
-                            #   system: Windows                                                              #
-                            #   version: 64bit                                                               #
-                            #                                       by: Bates <https://github.com/batestin1> #
+                            #   filename:{filename}.py                                                       #
+                            #   created: {data}                                                              #
+                            #                                                                                #
                             #********************************************************************************#
                             #                           import your librarys below                           #
                             #********************************************************************************#
 
 
-    from .{filename} import *
+from .{filename} import *
 
     """
     init = open(f"{filename}/{filename}/__init__.py", "w")
